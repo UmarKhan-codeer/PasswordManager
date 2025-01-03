f6e48b791655026b264ba6e0255f9e60be2291bb
UmarKhan-codeer <umarjavedkhan22@gmail.com>
2025-01-03 22:31:38 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Manager from './components/Manager'
+import Footer from './components/Footer'
+
+
+function App() {
+
+  return (
+    <>
+       <Navbar/>
+      <div className="bg-green-50 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)][background-size:16px_16px]">
+      <Manager/>
+      </div>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
